6927ac2edaeb0174b21115679130d140fd70a993
Junior Margalho <juniormargalho@brq.com>
2023-06-26 16:15:36 -0300

    feature: resolved reference Models

---
+++ BaseVIP/Scenes/Login/Models/LoginResponseModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginResponseModel.swift
+//  BaseVIP
+//
+//  Created by Junior Margalho on 26/06/23.
+//
+
+import Foundation
+
+struct LoginResponseModel {
+    let firstName: String
+    let lastName: String
+}
---
+++ BaseVIP/Scenes/Login/Models/LoginViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewModel.swift
+//  BaseVIP
+//
+//  Created by Junior Margalho on 26/06/23.
+//
+
+import Foundation
+
+struct LoginViewModel {
+    let titleAlertMessage: String
+    let titleAlertButton: String
+    let alertMessage: String
+}
